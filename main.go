916907a1f70615e99d3db8bd456a73b899c0a9f8
Itamar <freg10@gmail.com>
2021-05-22 21:10:26 +0300

    final

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"ow_test/entities"
-)
-
 func main() {
-	fmt.Println("asd")
-	var x1 = entities.Msg{
-		ID: "IDasd",
-		ParentID: "parent",
-		Data: "data",
-	}
-	fmt.Println(x1.ID)
+
 }
--- tree/tree.go
+++ tree/tree.go
@@ -2,23 +2,44 @@ package tree
 
 import (
 	"ow_test/entities"
+	"sync"
 )
 
-var g int
-
 type Tree struct {
-	children []*node
-	nodeMap  map[string]*node
+	children    []*node
+	nodeMap     map[string]*node
+	childrenMap map[string][]*node
+	mapLock     sync.Mutex
 }
 
 func NewTree() *Tree {
 	return &Tree{
-		children: make([]*node, 0),
-		nodeMap:  make(map[string]*node),
+		children:    make([]*node, 0),
+		nodeMap:     make(map[string]*node),
+		childrenMap: make(map[string][]*node),
 	}
 }
 
 func (t *Tree) Add(msg *entities.Msg) {
+	addedNode := newNode(msg)
+	t.mapLock.Lock()
+	defer t.mapLock.Unlock()
+	t.nodeMap[addedNode.ID] = addedNode
+	addedNode.children = t.childrenMap[addedNode.ID]
+	if addedNode.ParentID == `` {
+		t.children = append(t.children, addedNode)
+	}
+	t.childrenMap[addedNode.ParentID] = append(t.childrenMap[addedNode.ParentID], addedNode)
+	_, parentExists := t.nodeMap[addedNode.ParentID]
+	if parentExists {
+		t.nodeMap[addedNode.ParentID].children = t.childrenMap[addedNode.ParentID]
+	}
+}
+
+// An alternative Add implementation
+func (t *Tree) Add2(msg *entities.Msg) {
+	t.mapLock.Lock()
+	defer t.mapLock.Unlock()
 	addedNode := newNode(msg)
 	_, exists := t.nodeMap[addedNode.ID]
 	_, parentExists := t.nodeMap[addedNode.ParentID]
@@ -33,7 +54,7 @@ func (t *Tree) Add(msg *entities.Msg) {
 	}
 	if addedNode.ParentID == `` {
 		t.children = append(t.children, t.nodeMap[addedNode.ID])
-	} else { //not main leaf
+	} else { // not a first level node
 		t.nodeMap[addedNode.ParentID].children = append(t.nodeMap[addedNode.ParentID].children, t.nodeMap[addedNode.ID])
 	}
 }
